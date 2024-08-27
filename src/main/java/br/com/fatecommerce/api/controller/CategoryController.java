d3581ebfd758362a4e6902fb083540bad3566084
Tiago Ribeiro Carneiro <tiago.rib@gmail.com>
2024-08-27 20:43:27 -0300

    feat: implements swagger

--- src/main/java/br/com/fatecommerce/api/controller/CategoryController.java
+++ src/main/java/br/com/fatecommerce/api/controller/CategoryController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -26,6 +28,24 @@ public class CategoryController {
     public ResponseEntity<Object> saveCategory(@RequestBody Category category) {
         Category result = categoryService.saveCategory(category);
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    @DeleteMapping(value = "/delete/{idCategory}")
+    public ResponseEntity<Object> deleteCategory(@PathVariable Long idCategory) {
+        HashMap<String, Object> result = categoryService.deleteCategory(idCategory);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @GetMapping(value = "/findCategory/{idCategory}")
+    public ResponseEntity<Object> getCategoryById(@PathVariable Long idCategory){
+        Category result = categoryService.findCategoryById(idCategory);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @PutMapping(value = "/update")
+    public ResponseEntity<Object> updateCategory(@RequestBody Category category) {
+        Category result = categoryService.updateCategory(category);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
 }
--- src/main/java/br/com/fatecommerce/api/service/CategoryService.java
+++ src/main/java/br/com/fatecommerce/api/service/CategoryService.java
@@ -3,9 +3,13 @@ package br.com.fatecommerce.api.service;
 import br.com.fatecommerce.api.repository.CategoryRepository;
 import br.com.fatecommerce.api.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -19,6 +23,34 @@ public class CategoryService {
 
     public List<Category> getInfoCategories() {
         return categoryRepository.findAll();
+    }
+
+    public HashMap<String, Object> deleteCategory(Long idCategory) {
+        Optional<Category> category =
+                Optional.ofNullable(categoryRepository.findById(idCategory).
+                        orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                                "Categoria não encontrada!")));
+
+
+        categoryRepository.delete(category.get());
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        result.put("result", "Categoria: " + category.get().getNameCategory() +  " excluída com sucesso!");
+        return result;
+    }
+
+    public Category findCategoryById(Long idCategory) {
+        return categoryRepository.findById(idCategory)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Categoria não encontrada!"));
+    }
+
+    public Category updateCategory(Category category) {
+        if (findCategoryById(category.getIdCategory()) != null) {
+            return categoryRepository.saveAndFlush(category);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "Categoria não encontrada!");
+        }
     }
 
 }
